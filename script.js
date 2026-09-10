7e7a565a8a0dd4be0a6384fafe2b7bbb5f544f07
kanak chouksey <kanakchouksey53@gmail.com>
2026-09-10 20:29:20 +0530

    html is hydrate by js

--- script.js
+++ script.js
@@ -1,3 +1,117 @@
-// create a function that adds two numbers
-function add(a, b) {
-    //
+const filters = {
+    brightness: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+
+    },
+    contrast: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    },
+
+    exposure: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    },
+
+    saturation: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+
+    },
+    hueRotation: {
+        value: 0,
+        min: 0,
+        max: 360,
+        unit: "deg"
+    },
+
+    blur: {
+        value: 0,
+        min: 0,
+        max: 20,
+        unit: "px"
+
+    },
+    grayscale: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+
+    }
+    ,
+    Sepia: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+
+
+    },
+    opacity: {
+        value: 100,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+    invert: {
+        value: 0,
+        min: 0,
+        max: 100,
+        unit: "%"
+    },
+
+
+
+
+};
+
+const imagecanvas = document.querySelector("#image-canvas");
+const imageinput = document.querySelector("#image-input");
+let filtersdiv =  document.querySelector(".filters");
+
+
+
+
+function createElement(name, unit = "%", value, min, max) {
+    const div = document.createElement("div");
+    div.classList.add("filter");
+
+    const input = document.createElement("input");
+    input.type = "range";
+    input.min = min;
+    input.max = max;
+    input.value = value;
+    input.id = name;
+
+    const p = document.createElement("p");
+    p.innerText = name;
+    div.appendChild(p);
+    div.appendChild(input);
+
+    return div;
+}
+
+
+Object.keys(filters).forEach(key =>{
+    const filterElement = createElement(key,filters[key].unit,filters[key].value,filters[key].min,filters[key].max);
+    filtersdiv.appendChild(filterElement);
+
+
+});
+
+
+imageinput.addEventListener("change" ,(event)=>{
+    const file = event.target.files[0];
+    console.log(file);
+
+});

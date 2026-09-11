cb7a043a520af6d23a2312378c35c888eece7968
kanak chouksey <kanakchouksey53@gmail.com>
2026-09-11 13:59:50 +0530

    canvas adn filter apply is build

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 const filters = {
     brightness: {
         value: 100,
         min: 0,
         max: 200,
         unit: "%"
-
     },
+
     contrast: {
         value: 100,
         min: 0,
@@ -20,14 +21,14 @@ const filters = {
         unit: "%"
     },
 
-    saturation: {
+    saturate: {
         value: 100,
         min: 0,
         max: 200,
         unit: "%"
-
     },
-    hueRotation: {
+
+    huerotate: {
         value: 0,
         min: 0,
         max: 360,
@@ -39,54 +40,57 @@ const filters = {
         min: 0,
         max: 20,
         unit: "px"
-
     },
+
     grayscale: {
         value: 0,
         min: 0,
         max: 100,
         unit: "%"
+    },
 
-    }
-    ,
     Sepia: {
         value: 0,
         min: 0,
         max: 100,
         unit: "%"
-
-
     },
+
     opacity: {
         value: 100,
         min: 0,
         max: 100,
         unit: "%"
     },
+
     invert: {
         value: 0,
         min: 0,
         max: 100,
         unit: "%"
-    },
-
-
-
-
+    }
 };
 
-const imagecanvas = document.querySelector("#image-canvas");
+
+let filtersdiv = document.querySelector(".filters");
+
 const imageinput = document.querySelector("#image-input");
-let filtersdiv =  document.querySelector(".filters");
 
+const imageCanvas = document.querySelector("#image-canvas");
+const Canvasctx = imageCanvas.getContext("2d");
 
+const placeholder = document.querySelector(".placeholder");
+
+let image = null;
 
 
 function createElement(name, unit = "%", value, min, max) {
+
     const div = document.createElement("div");
     div.classList.add("filter");
 
     const input = document.createElement("input");
+
     input.type = "range";
     input.min = min;
     input.max = max;
@@ -95,23 +99,79 @@ function createElement(name, unit = "%", value, min, max) {
 
     const p = document.createElement("p");
     p.innerText = name;
+
     div.appendChild(p);
     div.appendChild(input);
+
+
+    input.addEventListener("input", (event) => {
+
+        filters[name].value = Number(input.value);
+        
+        applyfilter(name);
+
+    });
+
 
     return div;
 }
 
 
-Object.keys(filters).forEach(key =>{
-    const filterElement = createElement(key,filters[key].unit,filters[key].value,filters[key].min,filters[key].max);
+Object.keys(filters).forEach(key => {
+
+    const filterElement = createElement(
+        key,
+        filters[key].unit,
+        filters[key].value,
+        filters[key].min,
+        filters[key].max
+    );
+
     filtersdiv.appendChild(filterElement);
 
-
 });
 
 
-imageinput.addEventListener("change" ,(event)=>{
+// Image input
+
+imageinput.addEventListener("change", (event) => {
+
     const file = event.target.files[0];
-    console.log(file);
+
+    const img = new Image();
+
+    img.src = URL.createObjectURL(file);
+
+
+    img.onload = () => {
+
+        image = img;
+
+        imageCanvas.style.display = "initial";
+
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+
+        Canvasctx.drawImage(img, 0, 0);
+
+        placeholder.style.display = "none";
+
+    }
 
 });
+
+
+function applyfilter(name) {
+let filterName = name;
+
+    if (name == "huerotate") {
+        filterName = "hue-rotate";
+    }
+
+
+    Canvasctx.filter =
+        `${filterName}(${filters[name].value}${filters[name].unit})`;
+
+    Canvasctx.drawImage(image, 0, 0);
+
+}
